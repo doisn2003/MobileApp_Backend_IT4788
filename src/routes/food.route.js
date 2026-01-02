17fc3d6174e9a9cd1b05f6762eaecfd6a9b59824
doisn2003 <ngocdoisamsung@gmail.com>
2026-01-02 23:31:13 +0700

    edit category & unit

--- src/routes/food.route.js
+++ src/routes/food.route.js
@@ -13,6 +13,12 @@ router.post('/', upload.single('image'), foodController.createFood);
 // GET /food/ -> Lấy danh sách
 router.get('/', foodController.getFoods);
 
+// GET /food/categories
+router.get('/categories', foodController.getAllCategories);
+
+// GET /food/units
+router.get('/units', foodController.getAllUnits);
+
 // PUT /food/ -> Cập nhật (có thể có ảnh)
 router.put('/', upload.single('image'), foodController.updateFood);
 
